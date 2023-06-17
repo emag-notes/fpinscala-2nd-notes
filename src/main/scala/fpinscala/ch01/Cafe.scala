a346af545b591e1f738b32ef5d90a40b541333ee
Yoshimasa Tanabe <yoshimasa_tanabe@funds.jp>
2023-06-17 14:27:12 +0900

    ch01: Payments を導入して実際の CreditCard の処理をせずにテストできるようにした

--- src/main/scala/fpinscala/ch01/Cafe.scala
+++ src/main/scala/fpinscala/ch01/Cafe.scala
@@ -1,14 +1,19 @@
 package fpinscala.ch01
 
 class Cafe:
-  def buyCoffee(cc: CreditCard): Coffee =
+  def buyCoffee(cc: CreditCard, p: Payments): Coffee =
     val cup = Coffee()
-    cc.charge(cup.price)
+    p.charge(cc, cup.price)
     cup
 
-class CreditCard:
-  def charge(price: Double): Unit =
-    println("charging " + price)
+class CreditCard
+
+trait Payments:
+  def charge(cc: CreditCard, price: Double): Unit
+
+class SimulatedPayments extends Payments:
+  override def charge(cc: CreditCard, price: Double): Unit =
+    println(s"charging $price to $cc")
 
 class Coffee:
   val price: Double = 2.0
--- src/test/scala/fpinscala/ch01/CafeSuite.scala
+++ src/test/scala/fpinscala/ch01/CafeSuite.scala
@@ -6,9 +6,10 @@ class CafeSuite extends FunSuite {
   test("buyCoffee") {
     // Given
     val cc = CreditCard()
+    val p = SimulatedPayments()
     val cafe = Cafe()
     // When
-    val cup = cafe.buyCoffee(cc)
+    val cup = cafe.buyCoffee(cc, p)
     // THen
     assertEquals(cup.price, 2.0D)
   }
