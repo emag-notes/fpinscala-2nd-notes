2726d2a9ca9ab172ae5d01039f2fb5825c36035e
Yoshimasa Tanabe <yoshimasa_tanabe@funds.jp>
2023-06-21 00:48:33 +0900

    ch02: Done chapter 2.3

--- src/main/scala/fpinscala/ch02/MyProgram.scala
+++ src/main/scala/fpinscala/ch02/MyProgram.scala
@@ -5,9 +5,27 @@ object MyProgram:
     if n < 0 then -n
     else n
 
-  private def formatAbs(x: Int) =
-    val msg = "The absolute value of %d id %d"
-    msg.format(x, abs(x))
+  private def factorial(n: Int): Int =
+    @annotation.tailrec
+    def go(n: Int, acc: Int): Int =
+      if n <= 0 then acc
+      else go(n - 1, n * acc)
 
-  @main def printAbs(): Unit =
-    println(formatAbs(-42))
+    go(n, 1)
+
+  private def fib(n: Int): Int =
+    @annotation.tailrec
+    def go(n: Int, current: Int, next: Int): Int =
+      if n <= 0 then current
+      else go(n - 1, next, current + next)
+
+    go(n, 0, 1)
+
+  private def formatResult(name: String, n: Int, f: Int => Int) =
+    val msg = "The %s of %d is %d."
+    msg.format(name, n, f(n))
+
+  @main def print(): Unit =
+    println(formatResult("absolute value", -42, abs))
+    println(formatResult("factorial", 7, factorial))
+    println(formatResult("fib", 6, fib))
